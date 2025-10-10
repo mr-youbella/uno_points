04f544f56255d48a073737db998812cfe5d6cc24
Younes Oubellal <younesoubllal@gmail.com>
2025-10-10 16:10:43 +0100

    add simple history

--- script.js
+++ script.js
@@ -23,16 +23,16 @@ function add_points()
 	let new_value;
 	if (form.player.value === "player1")
 	{
-		localStorage.last_name = name_p1;
 		value_cookie = localStorage.player1.split('/')[1].trim();
 		new_value = +value_cookie + +input.value;
+		localStorage.last_name = name_p1 + " / " + new_value;
 		localStorage.player1 = name_p1 + " / " + new_value;
 	}
 	else
 	{
-		localStorage.last_name = name_p2;
 		value_cookie = localStorage.player2.split('/')[1].trim();
 		new_value = +value_cookie + +input.value;
+		localStorage.last_name = name_p2 + " / " + new_value;
 		localStorage.player2 = name_p2 + " / " + new_value;
 	}
 	input.value = 0;
@@ -40,74 +40,6 @@ function add_points()
 
 result_player1.value = localStorage.player1.split('/')[1].trim();
 result_player2.value = localStorage.player2.split('/')[1].trim();
-
-let historyClass = 
-{
-	uno_1: "uno_null",
-	uno_2: "uno_null",
-	uno_3: "uno_null",
-	uno_4: "uno_null",
-	uno_5: "uno_null",
-	uno_6: "uno_null",
-	uno_7: "uno_null",
-	uno_8: "uno_null",
-	uno_9: "uno_null",
-	uno_skipEveryone: "uno_null",
-	uno_wild: "uno_null",
-	uno_wildDrawTwo: "uno_null",
-	uno_wildDrawColor: "uno_null",
-	uno_drawOne: "uno_null",
-	uno_drawFive: "uno_null",
-	uno_reverse: "uno_null",
-	uno_skip: "uno_null",
-	uno_flip: "uno_null",
-};
-
-function ft_historyClass(card_name, card_value)
-{
-	let allHistory = [];
-	if (card_name === "uno_1")
-	{
-		allHistory.push(card_name + "=" + card_value);
-		historyClass.uno_1 = card_value;
-	}
-	if (card_name === "uno_2")
-		historyClass.uno_2 = card_value;
-	if (card_name === "uno_3")
-		historyClass.uno_3 = card_value;
-	if (card_name === "uno_4")
-		historyClass.uno_4 = card_value;
-	if (card_name === "uno_5")
-		historyClass.uno_5 = card_value;
-	if (card_name === "uno_6")
-		historyClass.uno_6 = card_value;
-	if (card_name === "uno_7")
-		historyClass.uno_7 = card_value;
-	if (card_name === "uno_8")
-		historyClass.uno_8 = card_value;
-	if (card_name === "uno_9")
-		historyClass.uno_9 = card_value;
-	if (card_name === "uno_skipEveryone")
-		historyClass.uno_skipEveryone = card_value;
-	if (card_name === "uno_wild")
-		historyClass.uno_wild = card_value;
-	if (card_name === "uno_wild")
-		historyClass.uno_wild = card_value;
-	if (card_name === "uno_wildDrawColor")
-		historyClass.uno_wildDrawColor = card_value;
-	if (card_name === "uno_drawOne")
-		historyClass.uno_drawOne = card_value;
-	if (card_name === "uno_drawFive")
-		historyClass.uno_drawFive = card_value;
-	if (card_name === "uno_reverse")
-		historyClass.uno_reverse = card_value;
-	if (card_name === "uno_skip")
-		historyClass.uno_skip = card_value;
-	if (card_name === "uno_flip")
-		historyClass.uno_flip = card_value;
-	console.log(allHistory);
-	return (card_value);
-}
 
 let uno_1 = document.getElementById("uno-1");
 let uno_2 = document.getElementById("uno-2");
@@ -130,7 +62,7 @@ let uno_skip = document.getElementById("uno-skip");
 let uno_flip = document.getElementById("uno-flip");
 
 
-uno_1.onclick = () => (input.value = +input.value + ft_historyClass("uno_1", 1));
+uno_1.onclick = () => (input.value = +input.value + 1);
 uno_2.onclick = () => (input.value = +input.value + 2);
 uno_3.onclick = () => (input.value = +input.value + 3);
 uno_4.onclick = () => (input.value = +input.value + 4);
@@ -199,8 +131,8 @@ function showHistory()
 	Swal.fire
 	(
 		{
-			title: "Last history for " + localStorage.last_name,
-			text: "hello",
+			title: "Last history for " + localStorage.last_name.split(" / ")[0],
+			text: localStorage.last_name.split(" / ")[1] + " point",
 		}
 	);
 }
