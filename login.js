8a2c3c7c7e1cc971fdc63721bd3908b18d109ab4
Younes Oubellal <younesoubllal@gmail.com>
2025-10-23 00:20:44 +0100

    Add class in js's

--- login.js
+++ login.js
@@ -1,76 +1,73 @@
+// Check if there players
 if (localStorage.player1 && localStorage.player2)
 	location.replace("add_points.html");
 
-let form_names = document.getElementById("form_names");
-
-let name_p1 = document.getElementById("name_player1");
-let name_p2 = document.getElementById("name_player2");
-
-form_names.onsubmit = function (submit)
+//Class
+class Login
 {
-	submit.preventDefault();
-	localStorage.player1 = name_p1.value.split('').map((value) => (value >= 'a' && value <= 'z' || value >= 'A' && value <= 'Z') ? value : '').join("") + " / 0";
-	localStorage.player2 = name_p2.value.split('').map((value) => (value >= 'a' && value <= 'z' || value >= 'A' && value <= 'Z') ? value : '').join("") + " / 0";
-	location.replace("add_points.html");
+	// Private attribute
+	#form = document.getElementById("form_names");
+	#file = document.getElementById("file");
+	// Methods
+	input_login()
+	{
+		this.#form.onsubmit = function (submit)
+		{
+			let regular;
+
+			submit.preventDefault();
+			regular = /^[a-zA-Z\s]+$/;
+			if (regular.test(this.name_player1.value) && regular.test(this.name_player2.value) && this.name_player1.value.length <= 10 && this.name_player2.value.length <= 10)
+			{
+				localStorage.player1 = this.name_player1.value + " / 0";
+				localStorage.player2 = this.name_player2.value + " / 0";
+				location.replace("add_points.html");
+			}
+			else
+			{
+				Swal.fire
+				({
+					icon: "error",
+					title: "Invalid names",
+					text: "Enter name just letters and Spaces and max 10 letter",
+				})
+			}
+		};
+	}
+	file_login()
+	{
+		this.#file.addEventListener('change', (event) =>
+		{
+			const fileList = event.target.files[0];
+			if (!fileList.name.match(/.log$/))
+			{
+				Swal.fire
+				({
+					icon: "error",
+					title: "Oops...",
+					text: "just files .log",
+				});
+				return ;
+			}
+			const reader = new FileReader();
+			reader.readAsText(fileList);
+			reader.onload = (e) =>
+			{
+				let content = e.target.result.split('\n');
+				localStorage.player1 = content[0];
+				localStorage.player2 = content[1];
+			};
+			location.reload();
+		});
+		document.addEventListener("keydown", (event) => (event.key === 'Alt' ? file.click() : null));
+	}
 };
 
-let file = document.getElementById("file");
-file.addEventListener('change', (event) =>
-{
-	const fileList = event.target.files[0];
-	if (!fileList.name.match(/.log/))
-	{
-		Swal.fire
-		({
-  			icon: "error",
-  			title: "Oops...",
- 			text: "just files .log",
-		});
-		return ;
-	}
-	const reader = new FileReader();
-	reader.readAsText(fileList);
-	reader.onload = (e) =>
-	{
-		let content = e.target.result.split('\n');
-		localStorage.player1 = content[0];
-		localStorage.player2 = content[1];
-	};
-	location.reload();
-});
-
-let div = document.getElementById("main_div");
-
-function inputs(input)
-{
-	input.onfocus = function ()
-	{
-		if (div.classList.contains("bg-gray-200"))
-		{
-			div.classList.remove("bg-gray-200");
-			div.classList.add("bg-green-200");
-		}
-	}
-	input.onblur = function ()
-	{
-		if (div.classList.contains("bg-green-200"))
-		{
-			div.classList.add("bg-gray-200");
-			div.classList.remove("bg-green-200");
-		}
-	}
-}
-
-function add_file(e)
-{
-	if (e.key === 'Alt')
-		file.click();
-}
-
-document.addEventListener("keydown", add_file);
-
-inputs(name_p1);
-inputs(name_p2);
+let login = new Login;
+// Take name players by inputs
+login.input_login();
+// Take name players and points by file
+login.file_login();
 
 addEventListener("storage", (event) => 
 {
--- script.js
+++ script.js
@@ -1,110 +1,134 @@
+// Check if not there players
 if (!localStorage.player1 || !localStorage.player2)
 	location.replace("index.html");
 
-let name_p1 = localStorage.player1.split('/')[0].trim();
-let name_p2 = localStorage.player2.split('/')[0].trim();
-for (let i = 0; i < document.getElementsByTagName("label").length; i += 2)
-	document.getElementsByTagName("label")[i].textContent = name_p1;
-for (let i = 1; i < document.getElementsByTagName("label").length; i += 2)
-	document.getElementsByTagName("label")[i].textContent = name_p2;
-
-let form = document.getElementById("form");
-let input = document.getElementById("result");
-let reset = document.getElementById("reset");
-let result_player1 = document.getElementById("player1-result");
-let result_player2 = document.getElementById("player2-result");
-
-
-if (!localStorage.history)
-	localStorage.setItem("history", "");
-function add_points() {
-	let value_cookie;
-	let new_value;
-	if (form.player.value === "player1") {
-		value_cookie = localStorage.player1.split('/')[1].trim();
-		new_value = +value_cookie + +input.value;
-		localStorage.history = name_p1 + " / " + new_value;
-		localStorage.player1 = name_p1 + " / " + new_value;
-	}
-	else {
-		value_cookie = localStorage.player2.split('/')[1].trim();
-		new_value = +value_cookie + +input.value;
-		localStorage.history = name_p2 + " / " + new_value;
-		localStorage.player2 = name_p2 + " / " + new_value;
-	}
-	input.value = 0;
-}
-
-result_player1.value = localStorage.player1.split('/')[1].trim();
-result_player2.value = localStorage.player2.split('/')[1].trim();
-
-let uno_1 = document.getElementById("uno-1");
-let uno_2 = document.getElementById("uno-2");
-let uno_3 = document.getElementById("uno-3");
-let uno_4 = document.getElementById("uno-4");
-let uno_5 = document.getElementById("uno-5");
-let uno_6 = document.getElementById("uno-6");
-let uno_7 = document.getElementById("uno-7");
-let uno_8 = document.getElementById("uno-8");
-let uno_9 = document.getElementById("uno-9");
-let uno_skipEveryone = document.getElementById("uno-skipEveryone");
-let uno_wild = document.getElementById("uno-wild");
-let uno_wildDrawTwo = document.getElementById("uno-wildDrawTwo");
-let uno_wildDrawColor = document.getElementById("uno-wildDrawColor");
-
-let uno_drawOne = document.getElementById("uno-drawOne");
-let uno_drawFive = document.getElementById("uno-drawFive");
-let uno_reverse = document.getElementById("uno-reverse");
-let uno_skip = document.getElementById("uno-skip");
-let uno_flip = document.getElementById("uno-flip");
-
-
-uno_1.onclick = () => (input.value = +input.value + 1);
-uno_2.onclick = () => (input.value = +input.value + 2);
-uno_3.onclick = () => (input.value = +input.value + 3);
-uno_4.onclick = () => (input.value = +input.value + 4);
-uno_5.onclick = () => (input.value = +input.value + 5);
-uno_6.onclick = () => (input.value = +input.value + 6);
-uno_7.onclick = () => (input.value = +input.value + 7);
-uno_8.onclick = () => (input.value = +input.value + 8);
-uno_9.onclick = () => (input.value = +input.value + 9);
-addEventListener("keydown", (event) => 
+// Class
+class UnoPoints
 {
-	if (event.key == 1)
-		input.value = +input.value + 1;
-	if (event.key == 2)
-		input.value = +input.value + 2;
-	if (event.key == 3)
-		input.value = +input.value + 3;
-	if (event.key == 4)
-		input.value = +input.value + 4;
-	if (event.key == 5)
-		input.value = +input.value + 5;
-	if (event.key == 6)
-		input.value = +input.value + 6;
-	if (event.key == 7)
-		input.value = +input.value + 7;
-	if (event.key == 8)
-		input.value = +input.value + 8;
-	if (event.key == 9)
-		input.value = +input.value + 9;
-});
+	// Private attribute
+	#name_player1 = localStorage.player1.split('/')[0].trim();
+	#name_player2 = localStorage.player2.split('/')[0].trim();
+	#value_player1 = localStorage.player1.split('/')[1].trim();
+	#value_player2 = localStorage.player2.split('/')[1].trim();
+	#form = document.getElementById("form");
+	#player1_result = document.getElementById("player1-result");
+	#player2_result = document.getElementById("player2-result");
+	#input = document.getElementById("result");
+	#delete = document.getElementById("delete");
+	#history = document.getElementById("history");
+	#edit_1 = document.getElementById("edit_1");
+	#edit_2 = document.getElementById("edit_2");
+	#link = document.getElementById("download-result");
+	// Cards
+	#uno_1 = document.getElementById("uno-1");
+	#uno_2 = document.getElementById("uno-2");
+	#uno_3 = document.getElementById("uno-3");
+	#uno_4 = document.getElementById("uno-4");
+	#uno_5 = document.getElementById("uno-5");
+	#uno_6 = document.getElementById("uno-6");
+	#uno_7 = document.getElementById("uno-7");
+	#uno_8 = document.getElementById("uno-8");
+	#uno_9 = document.getElementById("uno-9");
+	#uno_skipEveryone = document.getElementById("uno-skipEveryone");
+	#uno_wild = document.getElementById("uno-wild");
+	#uno_wildDrawTwo = document.getElementById("uno-wildDrawTwo");
+	#uno_wildDrawColor = document.getElementById("uno-wildDrawColor");
+	#uno_drawOne = document.getElementById("uno-drawOne");
+	#uno_drawFive = document.getElementById("uno-drawFive");
+	#uno_reverse = document.getElementById("uno-reverse");
+	#uno_skip = document.getElementById("uno-skip");
+	#uno_flip = document.getElementById("uno-flip");
 
-uno_skipEveryone.onclick = () => (input.value = +input.value + 30);
-uno_wild.onclick = () => (input.value = +input.value + 40);
-uno_wildDrawTwo.onclick = () => (input.value = +input.value + 50);
-uno_wildDrawColor.onclick = () => (input.value = +input.value + 60);
-uno_drawOne.onclick = () => (input.value = +input.value + 10);
-uno_drawFive.onclick = () => (input.value = +input.value + 20);
-uno_reverse.onclick = () => (input.value = +input.value + 20);
-uno_skip.onclick = () => (input.value = +input.value + 20);
-uno_flip.onclick = () => (input.value = +input.value + 20);
+	// Constructor
+	constructor()
+	{
+		// Protect local Storage for not changed
+		addEventListener("storage", (event) => (localStorage[event.key] = event.oldValue));
+		// Add localStorage History without value
+		if (!localStorage.history)
+			localStorage.setItem("history", "");
+		// Add names players in labels
+		for (let i = 0; i < document.getElementsByTagName("label").length; i += 2)
+			document.getElementsByTagName("label")[i].textContent = this.#name_player1;
+		for (let i = 1; i < document.getElementsByTagName("label").length; i += 2)
+			document.getElementsByTagName("label")[i].textContent = this.#name_player2;
+		// Add points in result each players
+		this.#player1_result.value = localStorage.player1.split('/')[1].trim();
+		this.#player2_result.value = localStorage.player2.split('/')[1].trim();
+		// After Enter points to player
+		this.#form.onsubmit = this.#add_points_in_player.bind(this);
+		// Delete players dataeturn to login
+		this.#delete.onclick = this.#delete_players.bind(this);
+		// Show history last player win
+		this.#history.addEventListener("click", this.#showHistory);
+		// Edit name and point player
+		this.#edit_1.onclick = () => this.#edit_name_value("player1");
+		this.#edit_2.onclick = () => this.#edit_name_value("player2");
+		// Add points for each card
+		this.#uno_1.onclick = () => (this.#input.value = +this.#input.value + 1);
+		this.#uno_2.onclick = () => (this.#input.value = +this.#input.value + 2);
+		this.#uno_3.onclick = () => (this.#input.value = +this.#input.value + 3);
+		this.#uno_4.onclick = () => (this.#input.value = +this.#input.value + 4);
+		this.#uno_5.onclick = () => (this.#input.value = +this.#input.value + 5);
+		this.#uno_6.onclick = () => (this.#input.value = +this.#input.value + 6);
+		this.#uno_7.onclick = () => (this.#input.value = +this.#input.value + 7);
+		this.#uno_8.onclick = () => (this.#input.value = +this.#input.value + 8);
+		this.#uno_9.onclick = () => (this.#input.value = +this.#input.value + 9);
+		this.#uno_skipEveryone.onclick = () => (this.#input.value = +this.#input.value + 30);
+		this.#uno_wild.onclick = () => (this.#input.value = +this.#input.value + 40);
+		this.#uno_wildDrawTwo.onclick = () => (this.#input.value = +this.#input.value + 50);
+		this.#uno_wildDrawColor.onclick = () => (this.#input.value = +this.#input.value + 60);
+		this.#uno_drawOne.onclick = () => (this.#input.value = +this.#input.value + 10);
+		this.#uno_drawFive.onclick = () => (this.#input.value = +this.#input.value + 20);
+		this.#uno_reverse.onclick = () => (this.#input.value = +this.#input.value + 20);
+		this.#uno_skip.onclick = () => (this.#input.value = +this.#input.value + 20);
+		this.#uno_flip.onclick = () => (this.#input.value = +this.#input.value + 20);
+		// Add points for each card by keydown
+		addEventListener("keydown", this.#add_points_by_keydown.bind(this));
+		// Download file uno_points.log for logs players [name_player / point_player]
+		const content = this.#name_player1 + " / " + +this.#value_player1 + "\n" + this.#name_player2 + " / " + +this.#value_player2;
+		const blob = new Blob([content]);
+		this.#link.href = URL.createObjectURL(blob);
+		this.#link.download = "uno_points.log";
+	}
 
-form.onsubmit = add_points;
+	// Methods
+	#add_points_in_player(event)
+	{
+		let	value_cookie;
+		let	new_value;
 
-function ft_reset() {
-	Swal.fire(
+		if (this.#form.player.value === "player1")
 		{
+			value_cookie = localStorage.player1.split('/')[1].trim();
+			new_value = +value_cookie + +this.#input.value;
+			localStorage.history = this.#name_player1 + " / " + new_value;
+			localStorage.player1 = this.#name_player1 + " / " + new_value;
+		}
+		else if (this.#form.player.value === "player2")
+		{
+			value_cookie = localStorage.player2.split('/')[1].trim();
+			new_value = +value_cookie + +this.#input.value;
+			localStorage.history = this.#name_player2 + " / " + new_value;
+			localStorage.player2 = this.#name_player2 + " / " + new_value;
+		}
+		else
+		{
+			event.preventDefault();
+			Swal.fire
+			({
+				icon: "warning",
+				title: "No choice",
+				text: "Please choice one player atless",
+			});
+		}
+		this.#input.value = 0;
+	}
+
+	#delete_players()
+	{
+		Swal.fire
+		({
 			title: "Are you sure?",
 			text: "You won't be able to revert this!",
 			icon: "warning",
@@ -112,106 +136,81 @@ function ft_reset() {
 			confirmButtonColor: "#3085d6",
 			cancelButtonColor: "#d33",
 			confirmButtonText: "Yes, delete it!"
-		}).then((result) => {
+		}).then((result) =>
+		{
 			if (result.isConfirmed)
 			{
 				localStorage.removeItem("player1");
 				localStorage.removeItem("player2");
 				localStorage.removeItem("history");
-				Swal.fire(
-					{
-						title: "Deleted!",
-						text: "Your file has been deleted.",
-						icon: "success",
-					}
-				).then(() => (location.reload()));
+				Swal.fire
+				({
+					title: "Deleted!",
+					text: "Your file has been deleted.",
+					icon: "success",
+				}).then(() => (location.reload()));
 			}
 		});
-}
+	}
 
-let value_cookie = localStorage.player1.split('/')[1].trim();
-let value_cookie2 = localStorage.player2.split('/')[1].trim();
-const content = name_p1 + " / " + +value_cookie + "\n" + name_p2 + " / " + +value_cookie2;
-const blob = new Blob([content], { type: "text/plain" });
-const link = document.getElementById("d");
-link.href = URL.createObjectURL(blob);
-link.download = "uno_points.log";
-
-reset.onclick = ft_reset;
-
-let history = document.getElementById("history");
-
-function showHistory()
-{
-	if (!localStorage.history)
-		return;
-	Swal.fire
-		(
-			{
-				title: "Last history for " + localStorage.history.split(" / ")[0],
-				text: localStorage.history.split(" / ")[1] + " point",
-			}
-		);
-}
-
-history.addEventListener("click", showHistory);
-
-function change_name(ls_player)
-{
-	Swal.fire(
+	#showHistory()
 	{
-		title: "Change name",
-		input: "text",
-		showCancelButton: true,
-		confirmButtonText: "Change",
-	}).then(
-	(result) => 
+		if (!localStorage.history)
+			return ;
+		Swal.fire
+		({
+			title: "Last history for " + localStorage.history.split(" / ")[0],
+			text: localStorage.history.split(" / ")[1] + " point",
+		});
+	}
+
+	#edit_name_value(player)
 	{
-		if (result.isConfirmed)
-			localStorage[ls_player] = result.value + " / " + localStorage[ls_player].split(" / ")[1];
-		Swal.fire(
-		{
-			title: "Change point",
+		Swal.fire
+		({
+			title: "Change name",
 			input: "text",
 			showCancelButton: true,
 			confirmButtonText: "Change",
-		}).then(
-		(result) => 
+		}).then((result) => 
 		{
 			if (result.isConfirmed)
-				localStorage[ls_player] = localStorage[ls_player].split(" / ")[0] + " / " + result.value;
-			location.reload();
+				localStorage[player] = result.value + " / " + localStorage[player].split(" / ")[1];
+			Swal.fire
+			({
+				title: "Change point",
+				input: "text",
+				showCancelButton: true,
+				confirmButtonText: "Change",
+			}).then((result) => 
+			{
+				if (result.isConfirmed)
+					localStorage[player] = localStorage[player].split(" / ")[0] + " / " + result.value;
+				location.reload();
+			});
 		});
-	});
-
-}
-
-function ft_editValue(player) {
-	if (player === "player1")
-	{
-		result_player1.removeAttribute("readonly");
-		change_name("player1");
 	}
-	else
+	#add_points_by_keydown(event)
 	{
-		result_player2.removeAttribute("readonly");
-		change_name("player2");
+		if (event.key == 1)
+			this.#input.value = +this.#input.value + 1;
+		else if (event.key == 2)
+			this.#input.value = +this.#input.value + 2;
+		else if (event.key == 3)
+			this.#input.value = +this.#input.value + 3;
+		else if (event.key == 4)
+			this.#input.value = +this.#input.value + 4;
+		else if (event.key == 5)
+			this.#input.value = +this.#input.value + 5;
+		else if (event.key == 6)
+			this.#input.value = +this.#input.value + 6;
+		else if (event.key == 7)
+			this.#input.value = +this.#input.value + 7;
+		else if (event.key == 8)
+			this.#input.value = +this.#input.value + 8;
+		else if (event.key == 9)
+			this.#input.value = +this.#input.value + 9;
 	}
-}
-
-let edit_1 = document.getElementById("edit_1");
-let edit_2 = document.getElementById("edit_2");
-
-edit_1.onclick = () => ft_editValue("player1");
-edit_2.onclick = () => ft_editValue("player2");
-
-addEventListener("storage", (event) => (localStorage[event.key] = event.oldValue));
-
-document.getElementById("header").onclick = () =>
-{
-	let cards = document.getElementById("cards");
-	if (!cards.classList.contains("hidden"))
-		cards.classList.add("hidden");
-	else
-		cards.classList.remove("hidden");
 };
+
+let uno_points = new UnoPoints;
